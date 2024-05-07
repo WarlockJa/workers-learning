e455d539daa59702f46845a8ec97c725e7a7480e
warlockja <warlockja@gmail.com>
2024-05-07 12:16:59 +0300

    hono ai mistral text generation

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,35 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+import { Ai } from '@cloudflare/ai';
+import { Hono } from 'hono';
+import { env } from 'hono/adapter';
+
+const app = new Hono();
+
+// GET /?query="How is your day today?"
+app.get('/', async (c) => {
+	const { AI } = env(c);
+	const ai = new Ai(AI);
+
+	const content = c.req.query('query') || 'Tell me a joke about Cloudflare';
+
+	const messages = [
+		{ role: 'system', content: 'you are a helpful assistant' },
+		{ role: 'user', content },
+	];
+
+	const inputs = { messages };
+
+	const response = await ai.run('@cf/mistral/mistral-7b-instruct-v0.1', inputs);
+
+	return c.json(response);
+});
+
+export default app;
+
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// };
